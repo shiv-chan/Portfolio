eaa01bfcd35c7af43eaf2dbd170665a4a780795e
shiv-chan <kshibuya31@gmail.com>
2021-02-09 15:39:12 +0900

    Update the modal and menu links on the ovelay.
    Add the open and close function on the modal.
    Add the anchor links on the each menu links on the overlay.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburgerLine = document.querySelector('.hamburgerLine');
 const overlay = document.querySelector('.overlay');
+const starDiscription = document.querySelector('#starDiscription');
+const modal = document.querySelector('#modal');
+const modalClose = document.querySelector('#modal .close');
+const menuItems = document.querySelectorAll('.menuItem');
 let menuOpen = false;
 
 hamburger.addEventListener('click', () => {
@@ -15,4 +19,22 @@ hamburger.addEventListener('click', () => {
     hamburgerLine.classList.remove('open');
     menuOpen = false;
   }
+});
+
+for(let menuItem of menuItems){
+  menuItem.addEventListener('click', () => {
+    overlay.classList.remove('open');
+    overlay.classList.add('close');
+    hamburgerLine.classList.remove('open');
+    menuOpen = false;
+  });
+}
+
+
+starDiscription.addEventListener('click', () => {
+  modal.classList.add('showModal');
+});
+
+modalClose.addEventListener('click', () => {
+  modal.classList.remove('showModal');
 });
